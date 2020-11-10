90822de1be78ffa0a992e72192b595f45c7ef4d3
Xinyi Ma <xinyima0625@gmail.com>
2020-11-10 12:46:57 -0500

    render a map with Google maps

---
+++ frontend/src/shared/components/UIElements/Map.js
@@ -0,0 +1,25 @@
+import React, { useRef, useEffect } from 'react';
+// use Ref can be used to create references 
+// get a reference of a real DOM node
+import "./Map.css";
+
+const Map = props => {
+    const mapRef = useRef(); // a empty connection
+    
+    const { center, zoom } = props; 
+    useEffect(() => {
+        const map = new window.google.maps.Map(mapRef.current, {
+            center: center,
+            zoom: zoom
+        });
+        // create a new marker in the center of the map
+        new window.google.maps.Marker({ position: props.center, map: map});    
+    }, [center, zoom]);
+    
+    return (<div 
+    ref={mapRef} // where connection is established
+    className={`map ${props.className}`} 
+    style={props.style}></div>);
+};
+
+export default Map;
