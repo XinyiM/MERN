473b72f1f3801610073cf144ee4c619268eca58e
Xinyi Ma <xinyima0625@gmail.com>
2020-11-13 11:45:07 -0500

    minor issues

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -5,6 +5,7 @@ import Card from '../../shared/components/UIElements/Card.js';
 import './PlaceList.css';
 import './PlaceItem';
 import PlaceItem from './PlaceItem';
+import Button from '../../shared/components/FormElements/Button';
 
 const PlaceList = props => {
     // output a list of places
@@ -12,7 +13,7 @@ const PlaceList = props => {
         return <div className="place-list center">
             <Card>
                 <h2> No places found. Maybe create one?</h2>
-                <button>Share Place</button>
+                <Button to='/places/new/'>Share Place</Button>
             </Card>
         </div>
     }
